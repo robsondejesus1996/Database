d048bc519ce102a33394f3ce4c47d58e9d8ff879
Robson <robsondejesus1996@hotmail.com>
2019-10-30 20:54:38 -0300

    Criação dos pacotes model e util e criação da classe coluna

---
+++ br.com.udesc.database/src/br/udesc/model/Coluna.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.udesc.model;
+
+/**
+ *
+ * @author 09527658969
+ */
+public class Coluna {
+    private String  nome;  //20 bytes
+    private char    tipo;  //2 bytes
+    private int     tamanhoBytes; //4 bytes -- numero de bytes utilizado no momento do insert (varia quando for uma string)
+    //atributos assima utilizados no cabecalho da tabela
+    //total - 26 bytes
+    
+    private boolean desconsiderar;
+
+    public Coluna() { }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getTamanhoBytes() {
+        return tamanhoBytes;
+    }
+
+    public void setTamanhoBytes(int tamanhoBytes) {
+        this.tamanhoBytes = tamanhoBytes;
+    }
+
+    public boolean isDesconsiderar() {
+        return desconsiderar;
+    }
+
+    public void setDesconsiderar(boolean desconsiderar) {
+        this.desconsiderar = desconsiderar;
+    }
+    
+}
