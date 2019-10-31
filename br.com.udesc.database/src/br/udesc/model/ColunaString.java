fc9ddbc03b264b36e40350add020a5553310aaa8
Thiago Correia <thiagomoraescorreia@gmail.com>
2019-10-31 19:37:18 -0300

    Criação da classe modelo Coluna String

---
+++ br.com.udesc.database/src/br/udesc/model/ColunaString.java
@@ -0,0 +1,15 @@
+package br.udesc.model;
+
+public class ColunaString extends Coluna {
+
+    private String valor;
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+}
