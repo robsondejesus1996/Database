085d5a61f9a059c5d7fc12d899a3be5b37f74935
Thiago Correia <thiagomoraescorreia@gmail.com>
2019-10-31 00:12:02 -0300

    Criação da classe modelo coluna Int

---
+++ br.com.udesc.database/src/br/udesc/model/ColunaInt.java
@@ -0,0 +1,15 @@
+package br.udesc.model;
+
+public class ColunaInt extends Coluna {
+
+    private int valor;
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return this.valor;
+    }
+
+}
