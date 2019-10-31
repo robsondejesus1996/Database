328c88e8d6adce5aced3a39e74f2b2ef926b71d0
Thiago Correia <thiagomoraescorreia@gmail.com>
2019-10-30 23:32:23 -0300

    Criação da classe modelo Coluna Float

---
+++ br.com.udesc.database/src/br/udesc/model/ColunaFloat.java
@@ -0,0 +1,17 @@
+package br.udesc.model;
+
+
+public class ColunaFloat extends Coluna {
+    
+    private float valor;
+
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+}
