448ab76ba2c38012405a622e6e4b6eeccd310697
Robson de Jesus <robsondejesus1996@hotmail.com>
2019-11-02 14:19:44 -0300

    Alteração classe criarBaseDados, para restrição de caracteres inválidos.

--- br.com.udesc.database/src/br/udesc/manipArq/CriadorBaseDados.java
+++ br.com.udesc.database/src/br/udesc/manipArq/CriadorBaseDados.java
@@ -18,13 +18,33 @@ import javax.swing.JOptionPane;
  * @author Robson de Jesus e Thiago Correira
  */
 public class CriadorBaseDados {
-    
-    public void criarBaseDados(String nomeBaseDados) throws IOException{
+
+    public void criarBaseDados(String nomeBaseDados) throws IOException {
         if (UtilArquivos.encontrarDataBase(nomeBaseDados) != null) {
             JOptionPane.showMessageDialog(null, "Base informada ja existe");
             return;
         }
-        File novaBase = new File("DADOS\\"+nomeBaseDados+"\\");
+        if (nomeBaseDados.length() > 20) {
+            JOptionPane.showMessageDialog(null, "O nome da base pode possuir no maximo 20 caracteres");
+            return;
+        }
+        if (!nomeBaseDados.matches("[\\w]*")) {
+            JOptionPane.showMessageDialog(null, "O nome da base de dados possui caracteres invalidos");
+            return;
+        }
+        File novaBase = new File("DADOS\\" + nomeBaseDados + "\\");
         novaBase.mkdir();
     }
+
+    public String listarBaseDadosExistentes() {
+        String nomesDb = "";
+        File arqDados = new File("DADOS");
+        for (String string : arqDados.list()) {
+            nomesDb += string + "\n";
+        }
+        return nomesDb;
+    }
+    
+    
+
 }
