4eb517a2361d1ef3995df6b81f4cbe199f9bd291
Robson de Jesus <robsondejesus1996@hotmail.com>
2019-11-06 20:28:56 -0300

    Adicionado pacote principal, com a classe principal para executar o projeto.

---
+++ br.com.udesc.database/src/br/com/udesc/principal/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.udesc.principal;
+
+import br.udesc.tela.TelaDb;
+
+/**
+ *
+ * @author Rj
+ */
+public class Principal {
+    
+   public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaDb().setVisible(true);
+            }
+        });
+    }
+    
+}
