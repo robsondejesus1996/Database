2e6887defb776d0939886a52e0a0c8d702b29248
Robson de Jesus <robsondejesus1996@hotmail.com>
2019-11-10 12:51:06 -0300

    Alterado o nome do pacote principal, pois estava dando um erro.

--- br.com.udesc.database/src/br/udesc/principal/Principal.java
+++ br.com.udesc.database/src/br/udesc/principal/Principal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.udesc.principal;
+package br.udesc.principal;
 
 import br.udesc.tela.TelaDb;
 
